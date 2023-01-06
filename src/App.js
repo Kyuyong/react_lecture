a9254195619105a1be37284d7cd8341c4e297491
조규용 <mvpcouple@naver.com>
2023-01-06 16:27:08 +0900

    shoeshop 1st commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+// import logo from './logo.svg';
+import { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './App.css';
+import data from './data.js';
+
+
+function App() {
+
+  let [shoes] = useState(data);
+
+  return (
+    <div className="App">
+      
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Shopping</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#cart">Cart</Nav.Link>
+            
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].content}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].content}</p>
+          </div>
+        </div>
+      </div> 
+
+    </div>
+  );
+}
+
+export default App;
